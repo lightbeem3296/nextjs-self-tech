b5e02af8340f74eff2dbfa121cfad95188ea14d9
Billy Lights <alpha5611331@gmail.com>
2023-10-02 02:40:00 +0900

    fixing memory bugs ...

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -30,4 +30,4 @@ export function getStaticProps() {
 }
 
 export default AllPostsPage;
-                                                         
+                                                          
